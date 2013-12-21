9143310f211f84d6b02a4ec71ca54f3afc6cb431
cuginoAle <alessio.carnevale@gmail.com>
2013-12-21 13:57:45 +0100

    removing dbouncer dependency on mqHelper plugin

--- assets/mqHelper.js
+++ assets/mqHelper.js
@@ -1,28 +1,35 @@
 ;(function(){
 
-    // to set the breakpoints please look for #mqHelper in the css file
+
+  // to set the breakpoints please set the following in your css
+
+  /*media query helper (used to make JS aware of the current MQ)*/
+  // #mqHelper{                                 //mobile  
+  //   position: absolute;
+  //   left:-9999px;
+  //   height: 0px;
+  //   width: 0px;
+  // }
+  // @media screen and (min-width: 600px) {     //tablet
+  //   #mqHelper{
+  //     height: 1px;
+  //   }
+  // }
+
+  // @media screen and (min-width: 1025px) {    //desktop
+  //   #mqHelper{
+  //     height: 2px;
+  //   }
+  // }
+
+
+    var devices=["mobile","tablet","desktop"],
+        mqDiv=$("<div id='mqHelper'/>").appendTo("body");
 
     // todo: improve the following
     $.fn.getDevice=function(){
         var i=mqDiv.height();
-        return device[i];     
+        return devices[i];     
     }
-
-    var device=["mobile","tablet","desktop"],
-        mqDiv=$("<div id='mqHelper'/>").appendTo("body"),
-        doCheck=new deBouncer(100),
-        prevDevice=mqDiv.getDevice();
-
-    $(window)
-        .on("resize",function(e){
-            doCheck.execute(function(e){
-                var currentDevice=mqDiv.getDevice();
-                if(currentDevice!=prevDevice){
-                    mqDiv.trigger("mqChange", currentDevice);
-                    prevDevice=currentDevice;
-                }
-            },e)
-        })
-
 
 })();
